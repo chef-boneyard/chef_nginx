634018b6c4dd38749da77a2b1fbdcfe9c3e3ace5
Leonid Korokh <lkorokh@gmail.com>
2016-10-19 15:13:16 +0300

    revert only_if

--- definitions/nginx_site.rb
+++ definitions/nginx_site.rb
@@ -25,6 +25,7 @@ define :nginx_site, enable: true, timing: :delayed do
     template "#{node['nginx']['dir']}/sites-available/#{params[:name]}" do
       source params[:template]
       variables(params[:variables])
+      only_if { params[:template] }
       notifies :reload, 'service[nginx]', params[:timing]
     end
 
